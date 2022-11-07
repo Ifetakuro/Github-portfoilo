26bd425b0998efb4a61701dc5e49e9c0fc370c9c
Oluwafisayo Takuro <93978195+Ifetakuro@users.noreply.github.com>
2022-11-07 11:18:51 +0100

    initial commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import SEO from "../components/Seo";
 import image from "../image/welcome.svg";
 
 export const Home = () => {
   return (
     <div className="container">
+      <SEO
+        title="Github Portfolio"
+        description="Github portfolio page to show all github repositories"
+        name="Oluwafisayo Takuro"
+        type="article"
+      />
       <h1>Github Portfolio</h1>
       <div>Welcome to my github portfolio</div>
       <p>
